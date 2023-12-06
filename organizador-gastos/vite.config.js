7695c706d786727a2a1aed2b816f37ccd01e34e7
diamecjs <diame.cjs@gmail.com>
2023-12-06 18:56:13 -0300

    landing y login conectados

--- organizador-gastos/vite.config.js
+++ organizador-gastos/vite.config.js
@@ -1,6 +1,14 @@
 // vite.config.js
 import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vite';
+
 export default {
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      'firebase/auth': 'firebase/auth',
+    },
+  },
 };
