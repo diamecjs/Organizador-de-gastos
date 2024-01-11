d6884a16a8694bcae54d4d27e85eb647a9382183
diamecjs <diame.cjs@gmail.com>
2024-01-11 12:44:33 -0300

    funcion total de gastos

--- organizador-gastos/src/Components/ExpenseModal/ExpenseModal.jsx
+++ organizador-gastos/src/Components/ExpenseModal/ExpenseModal.jsx
@@ -82,7 +82,7 @@ function ExpenseModal({ onClose }) {
     };
     return (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75">
-            <div className="bg-white p-8 rounded-md">
+            <div className="bg-violet-500 p-8 rounded-md">
                 <div className="flex justify-end">
                     <button
                         onClick={onClose}
@@ -91,9 +91,9 @@ function ExpenseModal({ onClose }) {
                         ❌
                     </button>
                 </div>
-                <h2 className="text-2xl font-bold mb-4">Agregar Nuevo Gasto</h2>
+                <h2 className="text-2xl text-white font-bold mb-4">Agregar Nuevo Gasto</h2>
                 <div className="mb-4">
-                    <label className="block text-gray-700">Nombre:</label>
+                    <label className="block text-indigo-950">Nombre:</label>
                     <input
                         type="text"
                         name="name"
@@ -103,7 +103,7 @@ function ExpenseModal({ onClose }) {
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700">Detalle:</label>
+                    <label className="block text-indigo-950">Detalle:</label>
                     <textarea
                         name="detail"
                         value={expenseData.detail}
@@ -112,7 +112,7 @@ function ExpenseModal({ onClose }) {
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700">Monto:</label>
+                    <label className="block text-indigo-950">Monto:</label>
                     <input
                         type="text"
                         name="amount"
@@ -122,7 +122,7 @@ function ExpenseModal({ onClose }) {
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700">Imagen:</label>
+                    <label className="block text-indigo-950">Imagen:</label>
 
                     <input
                         id="image"
@@ -131,11 +131,20 @@ function ExpenseModal({ onClose }) {
                         onChange={handleUploadFile}
                         className="w-full border border-gray-300 p-2 rounded"
                     />
-                    <img id="preview-image" alt="Vista previa de imagen" src={expenseData.image} className="w-20 h-20" />
+
+                    {expenseData.image && (
+                        <img
+                            id="preview-image"
+                            alt="Vista previa de imagen"
+                            src={expenseData.image}
+                            className="w-20 h-20 mt-2"
+                        />
+                    )}
                 </div>
+
                 <button
                     onClick={handleAddExpense}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-indigo-950 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
                 >
                     Agregar Gasto
                 </button>
